e74cf05bef0b70f33e337b25f87d98e7af41a4e3
Alexgg420 <alexgg888999@gmail.com>
2023-10-20 09:56:27 +0200

    proyecto creado

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'conceptos_basicos_angular',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
